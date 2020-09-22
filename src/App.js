19c1f3152ef1f207dadff6aff3e483fbeff8cbc8
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-09-22 14:45:06 -0300

    first commint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const api ={
 
+  key:"1ebb0130875c9e321fcfa0ffe0822e51",
+  base:"https:/api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>O PAI TA ON!</h1>
     </div>
   );
 }
