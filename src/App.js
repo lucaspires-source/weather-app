33015205ffb57265076f5ed679605f7475a84dd7
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-09-22 16:00:03 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,19 @@ function App() {
   
   const [query,setQuery] = useState('')
   const [weather,setWeather] = useState({})
-
+  
+  const search = e => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+  
   const dateBuilder = (d) =>{
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -31,21 +43,29 @@ function App() {
               type="text" 
               className="search-bar" 
               placeholder="Search..."
+              onChange={e => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
             />
 
           </div>
-          <div className="location-box">
-            <div className="location">Brasília,BR</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              28°C
+          {(typeof weather.main != "undefined") ? (
+            <div>
+              <div className="location-box">
+                <div className="location">{weather.name},{weather.sys.country}</div>
+                <div className="date">{dateBuilder(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                  <div className="temp">
+                  28°C
+                  </div>
+                  <div className="weather">Sunny</div>
             </div>
-            <div className="weather">Sunny</div>
-          </div>
+          </div> 
+           ) : () }
         </main>
     </div>
+
   );
 }
 
